fa63f6386fa78a44172cc324ee4f2fd4651e69cf
Juha Kajava <juhakaja@gmail.com>
2012-06-25 21:44:20 +0900

    Add time and message to melog -class.

--- lib/melog.rb
+++ lib/melog.rb
@@ -1,2 +1,12 @@
 class Melog
+  attr_accessor :time, :message 
+
+  def initialize(message)
+    @time = Time.now
+    @message = message
+  end
+
+  def to_s
+    "[#{@time}] #{@message}"
+  end
 end
--- spec/melog_spec.rb
+++ spec/melog_spec.rb
@@ -2,7 +2,29 @@ require 'spec_helper'
 
 describe Melog do
 
+  before :each do
+    @melog = Melog.new 'foo bar'
+  end
+
   it 'should be able to create an instance' do
-    Melog.new.should_not be_nil
+    @melog.should_not be_nil
+  end
+
+  it 'should have changeable date stamp' do
+    @melog.time.should be_kind_of Time
+    @melog.time.should be_within(0.01).of Time.now
+
+    new_time = Time.now - 60
+    @melog.time = new_time
+    @melog.time.should be new_time
+  end
+
+  it 'should have a message' do
+    @melog.message.should == 'foo bar'
+  end
+
+  it 'should display time with message using to_s' do
+    time = @melog.time
+    @melog.to_s.should == "[#{time}] foo bar"
   end
 end
